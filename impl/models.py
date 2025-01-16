0af40dbd1bbf61eed1376665cc389298b47d04b9
nkdem <dev@nkdem.net>
2025-01-16 09:13:13 +0000

    Update model

--- impl/models.py
+++ impl/models.py
@@ -6,8 +6,8 @@ class AudioCNN(nn.Module):
         # First CNN module
         self.cnn1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=cnn1_channels, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3)),
+            nn.BatchNorm2d(cnn1_channels),  # Added normalization layer
             nn.ReLU(),
-            nn.BatchNorm2d(cnn1_channels),
             nn.MaxPool2d(kernel_size=(5, 5), stride=(5, 5)),
             nn.Dropout(0.3)
         )
@@ -15,34 +15,20 @@ class AudioCNN(nn.Module):
         # Second CNN module
         self.cnn2 = nn.Sequential(
             nn.Conv2d(in_channels=cnn1_channels, out_channels=cnn2_channels, kernel_size=(7, 7), stride=(1, 1), padding=(3, 3)),
+            nn.BatchNorm2d(cnn2_channels),  # Added normalization layer
             nn.ReLU(),
-            nn.BatchNorm2d(cnn2_channels),
             nn.MaxPool2d(kernel_size=(4, 100), stride=(4, 100)),
             nn.Dropout(0.3)
         )
 
         # Fully connected module
         self.fc = nn.Sequential(
-            nn.Linear(cnn2_channels * 2 * 1, fc_neurons),  
+            nn.Linear(cnn2_channels * 2 * 1, fc_neurons),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(fc_neurons, num_classes),  # Final output layer
+            nn.Linear(fc_neurons, num_classes),
             nn.Softmax(dim=1)
         )
-        # self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_uniform_(m.weight, a=0.1, mode='fan_in', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.kaiming_uniform_(m.weight, a=0.1, mode='fan_in', nonlinearity='relu')
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = x.to(next(self.parameters()).device)
